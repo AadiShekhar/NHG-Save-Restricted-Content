a8fd110a6bb5d256290fec9bf455f893915bdece
AadiShekhar <68055024+AadiShekhar@users.noreply.github.com>
2025-06-04 11:13:44 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -2,35 +2,49 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-HELP_TXT = """**🌟 Help Menu** 
+HELP_TXT = """**🚀 Turbo Save Restricted Content Bot - Help Menu** 
 
-**__FOR PRIVATE CHATS__**
+This enhanced version downloads content much faster with parallel processing!
 
-__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__
+**__BASIC USAGE__**
+- First /login with your account
+- Send the Telegram message link you want to download
+- For multiple messages, use the range format (e.g., `100-120`)
 
+**__SUPPORTED LINK FORMATS__**
 
-**__FOR BOT CHATS__**
+1. **Public Channels:**
+   ```
+   https://t.me/channelname/123
+   https://t.me/channelname/100-120
+   ```
 
-__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
+2. **Private Chats:**
+   ```
+   https://t.me/c/123456789/123
+   https://t.me/c/123456789/100-120
+   ```
 
-```
-https://t.me/b/botusername/4321
-```
+3. **Bot Chats:**
+   ```
+   https://t.me/b/botusername/123
+   ```
 
-**__MULTI POSTS__**
+**__BATCH DOWNLOADS__**
+- Use the `from-to` format to download multiple messages
+- Example: `https://t.me/channelname/100-120` downloads messages 100 through 120
+- Maximum batch size: 100 messages
 
-__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
+**__OTHER COMMANDS__**
+- /start - Show bot introduction
+- /help - Show this help message
+- /login - Login with your Telegram account
+- /logout - Logout from current session
+- /cancel - Cancel ongoing downloads
 
+**Note:** The bot now processes up to 5 downloads simultaneously for faster performance!
+"""
 
-```
-https://t.me/xxxx/1001-1010
-
-https://t.me/c/xxxx/101 - 120
-```
-
-__note that space in between doesn't matter__"""
-       
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
